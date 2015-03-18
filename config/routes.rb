371c055131050d03970502cb73dd4ecfd7f2d894
shiro16 <nyanyanyawan24@gmail.com>
2015-03-18 20:05:38 +0900

    Add sample routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,83 +2,84 @@
 class PcConstraint 
   def matches?(request)
     # ua check
-		# uaがpcの場合にtrueを返す
-		true
+    # uaがpcの場合にtrueを返す
+    true
   end
 end
 
 Rails.application.routes.draw do
-  # get '/users',      to: 'users#index'
-  # get '/users/:id',  to: 'users#show'
-  # get '/users/test', to: 'users#test', as: :test
+  resources :users
+
+  get '/games',      to: 'games#index'
+  get '/games/:id',  to: 'games#show'
+  get '/games/test', to: 'games#test', as: :test
 
   # get '/users'      => 'users#index'
   # get '/users/:id'  => 'users#show'
   # get '/users/test' => 'users#test', as: :test
 
-  # resources :users
+  resource :posts, except: [:index, :show]
 
-  # resource :posts, except: [:index, :show]
+  resource :hoge, controller: :users
 
-  # resource :hoge, controller: :users
+  resource :authors do
+    resource :books
+  end
 
-  # resource :users do
-  #    resource :posts
-  # end
+  resources :dogs do
+    collection do
+      get 'test1'
+      get 'test2'
+    end
+  end
 
-  # resources :users do
-  #   collection do
-  #     get 'test1'
-  #     get 'test2'
-  #   end
-  # end
-
-  # resources :users do
-  #   member do
-  #     get 'test1'
-  #     get 'test2'
-  #   end
-  # end
+  resources :cats do
+    member do
+      get 'test3'
+      get 'test4'
+    end
+  end
 	
-	# resources :users, only: [:index, :show, :create] do
-  #   new do
-  #     get 'test1'
-  #     get 'test2'
-  #   end
-	# end
+  resources :companies, only: [:index, :show, :create] do
+    new do
+      get 'test1'
+      get 'test2'
+    end
+	end
 
-  # resources :hoge, only: [:index, :show], param: :user_id
+  resources :hoge, only: [:index, :show], param: :user_id
 
 
-  # namespace :admin do
-  #   resources :users, only: [:index, :show]
-  # end
+  namespace :admin do
+    resources :users, only: [:index, :show]
+  end
 
-  # scope module: 'admin' do
-  #   resources :users, only: [:index, :show]
-  # end
+  scope module: 'admin' do
+    resources :authors, only: [:index, :show]
+  end
 
-  # concern :commentable do
-  #   resources :comments, only: [:index, :show] do
-  #     get 'test'
-	# 	end
-  # end
+  concern :categorize do
+    resources :large_categories, only: [:index, :show] do
+      get 'test'
+	  end
+  end
 
-  # resources :users, only: [:index], concerns: :commentable
-  # resources :posts, only: [:index], concerns: :commentable
+  resources :dogs, only: [:index], concerns: :categorize
+  resources :cats, only: [:index], concerns: :categorize
 
-  # resources :users, constraints: {id: /\d+/}
+  resources :small_categories, constraints: {id: /\d+/}
 
-  # resources :users, only: [:index, :show], constraints: {subdomain: 'admin'}
+  resources :books, only: [:index, :show], constraints: {subdomain: 'admin'}
 
-  # get '/', to: 'pc#index', constraints: PcConstraint.new
-	# get '/', to: 'iphone#index', constraints: lambda {|request| request.user_agent =~ /iPhone/}
+  get '/', to: 'pc#index', constraints: PcConstraint.new
+	get '/', to: 'iphone#index', constraints: lambda {|request| request.user_agent =~ /iPhone/}
 
-  # get '/users/:id/default' => 'users#default', defaults: { format: :json }
-  # get '/posts/:id/default' => 'posts#default', defaults: { default_value: :hoge }
+  get '/users/:id/default' => 'users#default', defaults: { format: :json }
+  get '/posts/:id/default' => 'posts#default', defaults: { default_value: :hoge }
 
-  # resources :users, only: [:index, :show]
-  # get "user"     => redirect("/users")
-  # get "user/:id" => redirect("/users/%{id}")
-  # get 'user/*other' => redirect { |params| "/users/#{params[:other]}"}
+  resources :sports, only: [:index, :show]
+  
+  get "user"     => redirect("/users")
+  get "user/:id" => redirect("/users/%{id}")
+  get 'user/*other' => redirect { |params| "/users/#{params[:other]}"}
 end
